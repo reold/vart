43a09a9ac5ec1801bdbe3deb126cd74e31f579ec
reold <redicrafty@gmail.com>
2026-07-29 23:44:19 +0530

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import tailwindcss from "@tailwindcss/vite";
+import adapter from "@sveltejs/adapter-static";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    sveltekit({
+      compilerOptions: {
+        // Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+        runes: ({ filename }) =>
+          filename.split(/[/\\]/).includes("node_modules") ? undefined : true,
+      },
+
+      // configuring the static adapter for all-time SSG
+      adapter: adapter({
+        pages: "build",
+        assets: "build",
+        fallback: undefined,
+        precompress: false,
+        strict: true,
+      }),
+
+      paths: {
+        base: process.env.BASE_PATH || "",
+      },
+    }),
+  ],
+});
